99f685b781b09e64542fc80ce9c00e9d1bfd96e4
Beto Dealmeida <roberto@dealmeida.net>
2023-12-12 18:44:47 -0500

    feat: basic DB crud

--- src/byodb/__init__.py
+++ src/byodb/__init__.py
@@ -2,8 +2,11 @@
 Main BYODB application.
 """
 
+import asyncio
 from dataclasses import dataclass
+from pathlib import Path
 
+import aiosqlite
 from importlib.metadata import version
 from quart import Quart
 from quart_schema import QuartSchema, validate_response
@@ -14,6 +17,29 @@ app = Quart(__name__)
 app.register_blueprint(databases.blueprint)
 
 QuartSchema(app)
+
+app.config.update(
+    {
+        "DATABASE": Path(app.root_path) / "byodb.sqlite",
+    }
+)
+
+
+async def init_db() -> None:
+    """
+    Create tables.
+    """
+    async with aiosqlite.connect(app.config["DATABASE"]) as db:
+        with open(Path(app.root_path) / "schema.sql", mode="r") as file_:
+            await db.executescript(file_.read())
+        await db.commit()
+
+
+def init_db_sync():
+    """
+    Synchronous wrapper of `init_db` for Poetry.
+    """
+    asyncio.run(init_db())
 
 
 @dataclass
--- src/byodb/blueprints/databases/__init__.py
+++ src/byodb/blueprints/databases/__init__.py
@@ -5,9 +5,11 @@ These endpoints are used for CRUD operations on databases.
 """
 
 from dataclasses import dataclass
+from datetime import datetime
 from enum import StrEnum
 from uuid import UUID, uuid4
 
+import aiosqlite
 from quart import Blueprint
 from quart_schema import validate_request, validate_response
 
@@ -24,6 +26,8 @@ class Database:
     dialect: DialectEnum
     name: str
     description: str
+    created_at: datetime
+    last_modified_at: datetime
 
 
 @dataclass
@@ -47,18 +51,111 @@ class DatabaseResponse:
 @blueprint.route("/", methods=["GET"])
 @validate_response(DatabasesResponse)
 async def get_databases() -> DatabasesResponse:
-    return DatabasesResponse(result=[])
+    """
+    List all databases.
+    """
+    # XXX create_app
+    from byodb import app
+
+    result = []
+    async with aiosqlite.connect(app.config["DATABASE"]) as db:
+        async with db.execute(
+            "SELECT uuid, dialect, name, description, created_at, last_modified_at "
+            "FROM database"
+        ) as cursor:
+            async for (
+                uuid,
+                dialect,
+                name,
+                description,
+                created_at,
+                last_modified_at,
+            ) in cursor:
+                result.append(
+                    Database(
+                        uuid=UUID(uuid),
+                        dialect=DialectEnum(dialect),
+                        name=name,
+                        description=description,
+                        created_at=datetime.fromisoformat(created_at),
+                        last_modified_at=datetime.fromisoformat(last_modified_at),
+                    )
+                )
+
+    return DatabasesResponse(result=result)
+
+
+@blueprint.route("/<uuid:uuid>", methods=["GET"])
+@validate_response(DatabaseResponse)
+async def get_database(uuid: UUID) -> DatabaseResponse:
+    """
+    List a given databases.
+    """
+    # XXX create_app
+    from byodb import app
+
+    async with aiosqlite.connect(app.config["DATABASE"]) as db:
+        async with db.execute(
+            "SELECT uuid, dialect, name, description, created_at, last_modified_at "
+            "FROM database WHERE uuid = ?",
+            (str(uuid),),
+        ) as cursor:
+            (
+                uuid,
+                dialect,
+                name,
+                description,
+                created_at,
+                last_modified_at,
+            ) = await cursor.fetchone()
+            return DatabasesResponse(
+                result=Database(
+                    uuid=UUID(uuid),
+                    dialect=DialectEnum(dialect),
+                    name=name,
+                    description=description,
+                    created_at=datetime.fromisoformat(created_at),
+                    last_modified_at=datetime.fromisoformat(last_modified_at),
+                )
+            )
 
 
 @blueprint.route("/", methods=["POST"])
 @validate_request(DatabaseIn)
 @validate_response(DatabaseResponse)
 async def create_database(data: DatabaseIn) -> DatabaseResponse:
+    """
+    Create a database.
+    """
+    # XXX create_app
+    from byodb import app
+
+    uuid = data.uuid or uuid4()
+    created_at = last_modified_at = datetime.utcnow()
+
+    async with aiosqlite.connect(app.config["DATABASE"]) as db:
+        await db.execute(
+            "INSERT INTO database "
+            "(uuid, dialect, name, description, created_at, last_modified_at) "
+            "VALUES (?, ?, ?, ?, ?, ?)",
+            (
+                str(uuid),
+                data.dialect,
+                data.name,
+                data.description,
+                created_at,
+                last_modified_at,
+            ),
+        )
+        await db.commit()
+
     return DatabaseResponse(
         result=Database(
-            uuid=data.uuid or uuid4(),
+            uuid=uuid,
             dialect=data.dialect,
             name=data.name,
             description=data.description,
+            created_at=created_at,
+            last_modified_at=last_modified_at,
         )
     )
